e649f62579e08a9e048abfd6dca076896a899fbc
mbmetcalfe <30930387+mbmetcalfe@users.noreply.github.com>
2017-12-01 09:33:38 -0400

    Update abbot.py

--- abbot.py
+++ abbot.py
@@ -773,7 +773,7 @@ class Abbot(discord.Client):
             await self.add_roles(member, role)
             
             await self.safe_send_message(channel, fmt.format(member, server))
-        except discord.Forbidden
+        except discord.Forbidden:
             await client.send_message(message.channel, "Lol I can't add roles bro.")
         # check that they are joining 'this' server
         #if member.guild.id != 379363572876181515:
