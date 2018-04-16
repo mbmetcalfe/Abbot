abb823e39b0315e1d03207ca172d8a6015fd6cf5
Michael Metcalfe <michael.metcalfe@vac-acc.gc.ca>
2018-04-16 15:30:26 -0300

    Adding summarizeCommandUsage and summarizeMessageUsage

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import sqlite3
 import datetime
 import random
 import string
+import logging
 
 DATABASE_NAME = 'abbot.sqlite3'
 
@@ -13,7 +14,7 @@ def insertUpdateUsageCommand(user, server, channel, commandName, valid):
     values = ()
     sql = ""
 
-    #print("insertUpdateCommand({0}, {1}, {2}, {3}, {4})".format(user, server, channel, commandName, valid))
+    logger.debug("insertUpdateCommand(user={0}, server={1}, channel={2}, commandName={3}, valid={4})".format(user, server, channel, commandName, valid))
     # First check if the user has used this command on the server/channel
     values = (user, server, channel, commandName)
 
@@ -37,7 +38,7 @@ def insertUpdateUsageCommand(user, server, channel, commandName, valid):
         conn.commit()
 
     except BaseException as ex:
-        print("There was a problem inserting or updating the database record: {0}".format(ex))
+        logger.error("There was a problem inserting or updating the database record: {0}".format(ex))
     finally:
         conn.close()
 
@@ -52,7 +53,7 @@ def insertUpdateUsageMessage(user, server, channel, message):
     maxLength = characterCount
     ts = datetime.datetime.now()
 
-    #print("insertUpdateUsageMessage({0}, {1}, {2}, {3})".format(user, server, channel, message))
+    logger.debug("insertUpdateUsageMessage(user={0}, server={1}, channel={2}, message={3})".format(user, server, channel, "***"))
 
     # First check if the user has sent any messages on the server/channel
     values = (user, server, channel)
@@ -80,9 +81,146 @@ def insertUpdateUsageMessage(user, server, channel, message):
         conn.commit()
 
     except BaseException as ex:
-        print("There was a problem inserting or updating the database record: {0}".format(ex))
+        logger.error("There was a problem inserting or updating the database record: {0}".format(ex))
     finally:
         conn.close()
+
+def summarizeCommandUsage(server, channel, user, breakdown):
+    """ This function is used to summarize the command usage for the given server, channel, and user."""
+    values = []
+    sql = 'select user, sum(count) as command_calls from usage_commands group by user'
+
+    logger.debug("summarizeCommandUsage(server={0}, channel={1}, user={2}, breakdown={3})".format(server, channel, user, breakdown))
+    # For now, assume that server is a required parameter -- all queries should relate
+    # to the current server.
+    if server == None:
+        logger.info("Not implemented.")
+        return
+
+    if channel == None and user == None:
+        values = [server]
+        # Details for all commands
+        if breakdown:
+            sql = """select command_name, sum(count) as command_calls 
+                from usage_commands where server = ? 
+                group by command_name 
+                order by 2 desc, command_name"""
+        else:
+            sql = """select sum(count) as command_calls from usage_commands where server = ? order by 1 desc"""
+
+    elif channel == None and user != None:
+        values = [server, user]
+        # Details for a user on all channels
+        if breakdown:
+            sql = """select command_name, sum(count) as command_calls 
+                from usage_commands where server = ? and user = ? 
+                group by user, command_name order by 2 desc, command_name"""
+        else:
+            sql = """select sum(count) as command_calls 
+                from usage_commands where server = ? and user = ? 
+                group by user 
+                order by 1 desc"""
+
+    if channel != None and user == None:
+        values = [server, channel]
+        # Details for all commands from all users in a channel
+        if breakdown:
+            sql = """select command_name, sum(count) as command_calls 
+                from usage_commands where server = ? and channel = ? 
+                group by command_name order by 2 desc, command_name"""
+        else:
+            sql = """select sum(count) as command_calls 
+                from usage_commands where server = ? and channel = ? 
+                order by 1 desc"""
+
+    elif channel != None and user != None:
+        values = [server, user]
+        # Details for a user on a channels
+        if breakdown:
+            sql = """select command_name, sum(count) as command_calls 
+                from usage_commands where server = ? and channel = ? and user = ? 
+                group by user, command_name 
+                order by 2 desc, command_name"""
+        else:
+            sql = """select sum(count) as command_calls 
+                from usage_commands where server = ? and channel = ? and user = ? 
+                group by user order by 1 desc"""
+
+    elif channel != None and user == None:
+        sql = 'select user, sum(count) as command_calls from usage_commands'
+        values = []
+        
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    # logger.debug("SQL = {0}".format(sql))
+    # logger.debug("values = {0}".format(values))
+    cur.execute(sql, values)
+    columns = [i[0] for i in cur.description]
+    logger.debug('|{0}|'.format('\t\t\t|'.join(columns)))
+    for row in cur:
+        rowStr = '|'
+        for col in columns:
+            rowStr += ("{0}\t\t\t|".format(row[col]))
+        logger.debug(rowStr)
+
+def summarizeMessageUsage(server, channel, user):
+    """ This function is used to summarize the messages for the given server, channel, and user."""
+    values = []
+    sql = """select user, word_count, character_count, max_message_length from usage_messages order by max_message_length desc, user where server = ?"""
+
+    logger.debug("summarizeMessageUsage(server={0}, channel={1}, user={2})".format(server, channel, user))
+    # For now, assume that server is a required parameter -- all queries should relate to the current server.
+    if server == None:
+        logger.info("Not implemented.")
+        return
+
+    if channel == None and user == None:
+        values = [server]
+        # Details for all channels and all users
+        sql = """select sum(word_count) as word_count, sum(character_count) as character_count, max(max_message_length) as max_message 
+            from usage_messages where server = ?"""
+
+    elif channel == None and user != None:
+        values = [server, user]
+        # Details for a user on all channels
+        sql = """select sum(word_count) as word_count, sum(character_count) as character_count, max(max_message_length) as max_message, max(last_message_timestamp) as last_message 
+            from usage_messages 
+            where server = ? and user = ?"""
+
+    if channel != None and user == None:
+        values = [server, channel]
+        # Details for all commands from all users in a channel
+        sql = """select sum(word_count) as word_count, sum(character_count) as character_count, max(max_message_length) as max_message, max(last_message_timestamp) as last_message 
+            from usage_messages 
+            where server = ? and user = ?"""
+
+    elif channel != None and user != None:
+        # Details for a user on a channel
+        values = [server, channel, user]
+        sql = """select sum(word_count) as word_count, sum(character_count) as character_count, max(max_message_length) as max_message, max(last_message_timestamp) as last_message 
+            from usage_messages 
+            where server = ? and channel = ? and user = ?"""
+
+    elif channel != None and user == None:
+        sql = 'select user, sum(count) as command_calls from usage_messages'
+        values = []
+        
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    # logger.debug("SQL = {0}".format(sql))
+    # logger.debug("values = {0}".format(values))
+    cur.execute(sql, values)
+    columns = [i[0] for i in cur.description]
+    logger.debug('|{0}|'.format('\t\t\t|'.join(columns)))
+    for row in cur:
+        rowStr = '|'
+        for col in columns:
+            rowStr += ("{0}\t\t\t|".format(row[col]))
+        logger.debug(rowStr)
 
 # Create table
 #c.execute('''CREATE TABLE stocks
@@ -94,37 +232,79 @@ def insertUpdateUsageMessage(user, server, channel, message):
 #print(c.fetchone())
 
 # Larger example that inserts many records at a time
-purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
-             ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
-             ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
-            ]
+# purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
+#              ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
+#              ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
+#             ]
 #c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
 #conn.commit
 if __name__ == '__main__':
-    conn = sqlite3.connect(DATABASE_NAME)
+    # Setup logging
+    logger = logging.getLogger('abbot')
+    logger.setLevel(logging.DEBUG)
 
-    c = conn.cursor()
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
 
-    insertUpdateUsageCommand('username', 'servername', 'channelname', 'command', 1)
-    insertUpdateUsageCommand('foo', 'servername', 'channelname', 'command', 1)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s %(name)-8s %(levelname)9s: %(message)s')
+    ch.setFormatter(formatter)
+
+    # add the handlers to logger
+    logger.addHandler(ch)
+
+    channels = ['main', 'other', 'another']
+    users = ['foo', 'bar', 'username', 'blahblah']
+    commands = ['help', 'joke', 'roll', 'pick', 'choose', 'rpsls', 'ping', 'whoami']
+    serverName = 'servername'
+
+    insertUpdateUsageCommand(users[random.randint(0, len(users)-1)], serverName, channels[random.randint(0, len(channels)-1)], commands[random.randint(0, len(commands)-1)], 1)
+    insertUpdateUsageCommand(users[random.randint(0, len(users)-1)], serverName, channels[random.randint(0, len(channels)-1)], commands[random.randint(0, len(commands)-1)], 1)
     message = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase + string.whitespace + string.punctuation, k=random.randint(4, 1024)))
-    insertUpdateUsageMessage('username', 'servername', 'channelname', message)
+    insertUpdateUsageMessage(users[random.randint(0, len(users)-1)], serverName, channels[random.randint(0, len(channels)-1)], message)
     message = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase + string.whitespace + string.punctuation, k=random.randint(4, 1024)))
-    insertUpdateUsageMessage('foo', 'servername', 'channelname', message)
+    insertUpdateUsageMessage(users[random.randint(0, len(users)-1)], serverName, channels[random.randint(0, len(channels)-1)], message)
 
-    print("Command usage:")
-    headings = ('user', 'server', 'channel', 'command', 'valid', 'count')
-    print(tuple(headings))
-    for row in c.execute('SELECT * FROM usage_commands ORDER BY server, user, channel, command_name'):
-        print(row)
+    logger.debug("================================================================")
+    logger.debug("Command Usage: All Servers, All Channels, All Users, No Breakdown")
+    logger.debug("================================================================")
+    summarizeCommandUsage(None, None, None, False)
+    logger.debug("================================================================")
+    logger.debug("Command Usage: One Server, All Channels, All Users, No Breakdown")
+    logger.debug("================================================================")
+    summarizeCommandUsage(serverName, None, None, False)
+    logger.debug("================================================================")
+    logger.debug("Command Usage: One Server, All Channels, All Users, With Breakdown")
+    logger.debug("================================================================")
+    summarizeCommandUsage(serverName, None, None, True)
+    logger.debug("================================================================")
+    logger.debug("Command Usage: One Server, All Channels, One User, No Breakdown")
+    logger.debug("================================================================")
+    summarizeCommandUsage(serverName, None, users[random.randint(0, len(users)-1)], False)
+    logger.debug("================================================================")
+    logger.debug("Command Usage: One Server, All Channels, One User, With Breakdown")
+    logger.debug("================================================================")
+    summarizeCommandUsage(serverName, None, users[random.randint(0, len(users)-1)], True)
 
-    print("Message usage:")
-    headings = ('user', 'server', 'channel', 'word_count', 'character_count', 'max_message_length', 'last_message_timestamp')
-    print(tuple(headings))
-    for row in c.execute('SELECT user, server, channel, word_count, character_count, max_message_length, last_message_timestamp FROM usage_messages ORDER BY server, user, channel, max_message_length'):
-        print(row)
-
-
-    # We can also close the connection if we are done with it.
-    # Just be sure any changes have been committed or they will be lost.
-    conn.close()
+    logger.debug("================================================================")
+    logger.debug("Message Usage: All Servers, All Channels, All Users")
+    logger.debug("================================================================")
+    summarizeMessageUsage(None, None, None)
+    logger.debug("================================================================")
+    logger.debug("Message Usage: One Servers, All Channels, All Users")
+    logger.debug("================================================================")
+    summarizeMessageUsage(serverName, None, None)
+    logger.debug("================================================================")
+    logger.debug("Message Usage: One Servers, All Channels, One Users")
+    logger.debug("================================================================")
+    summarizeMessageUsage(serverName, None, users[random.randint(0, len(users)-1)])
+    logger.debug("================================================================")
+    logger.debug("Message Usage: One Servers, One Channels, One Users")
+    logger.debug("================================================================")
+    summarizeMessageUsage(serverName, channels[random.randint(0, len(channels)-1)], users[random.randint(0, len(users)-1)])
+    
+    # TODO: Detect/count emojis.
+    # https://stackoverflow.com/questions/43146528/how-to-extract-all-the-emojis-from-text
+    # a_list = ['🤔 🙈 me así, bla es se 😌 ds 💕👭👙 👨‍👩‍👦‍👦']
+    #re.findall(r'[^\w\s,]', a_list[0])
